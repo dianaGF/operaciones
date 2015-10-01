afb4fc405ebe8f55028c48e67c470042c251df1a
root <root@DebianD>
2015-10-01 14:21:23 -0500

    private suma

--- suma.php
+++ suma.php
@@ -1,6 +1,23 @@
 <?php
-function sumar($num1, $num2){
-      return $num1 + $num2;
+class Operaciones{
+	public $num1,$num2,$resultado;
+
+	public function asignaValores($num1, $num2){
+		$this->num1 = $num1;
+		$this->num2 = $num2;
+
+	}
+
+	private	function sumar($num1, $num2){
+      $this->resultado = $this->num1 + $this->num2;
+    }		
+
+    public function imprimir($resultado){
+    	$this->sumar();
+   		return $this->resultado;
+    }
 }
-echo "El resultado: ".sumar(20, 5);
+$suma = new Operaciones;
+$suma->asignaValores(2,3);
+echo $suma->imprimir();
 ?>
