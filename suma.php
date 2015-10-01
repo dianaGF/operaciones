c12c131a1bea3a0e46421a0c08dfac4773354ab7
root <root@DebianD>
2015-10-01 15:01:42 -0500

    constuctor operaciones

--- suma.php
+++ suma.php
@@ -1,23 +1,45 @@
 <?php
 class Operaciones{
+
 	public $num1,$num2,$resultado;
 
-	public function asignaValores($num1, $num2){
+	function __construct($num1,$num2){
 		$this->num1 = $num1;
 		$this->num2 = $num2;
-
 	}
 
-	private	function sumar($num1, $num2){
-      $this->resultado = $this->num1 + $this->num2;
+	public	function sumar($num1, $num2){
+		$this->resultado = $this->num1 + $this->num2;
     }		
 
-    public function imprimir($resultado){
-    	$this->sumar();
-   		return $this->resultado;
+    public	function restar($num1, $num2){
+		$this->resultado = $this->num1 - $this->num2;
+    }
+
+    public	function multiplicar($num1, $num2){
+		$this->resultado = $this->num1 * $this->num2;
+    }
+
+    public function imprimir(){
+    	return $this->resultado;
     }
 }
-$suma = new Operaciones;
-$suma->asignaValores(2,3);
-echo $suma->imprimir();
+
+$suma = new Operaciones(5,3);
+$suma->sumar();
+echo "Suma: ".$suma->imprimir();
+echo "<br>";
+
+$resta = new Operaciones(5,3);
+$resta->restar();
+echo "Resta: ".$resta->imprimir();
+echo "<br>";
+
+$multiplica = new Operaciones(5,3);
+$multiplica->multiplicar();
+echo "Multiplica: ".$multiplica->imprimir();
+
+
+
+
 ?>
